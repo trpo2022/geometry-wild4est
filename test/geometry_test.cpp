ec0464082b8802dc49e23b1bd3d19f7d6fbf64ca
wild4est <DashaCherochenko@mail.ru>
2022-05-14 00:00:29 +0700

    Successfully writing tests

--- test/geometry_test.cpp
+++ test/geometry_test.cpp
@@ -5,15 +5,15 @@
 #include <libgeometry/P.h>
 #include <libgeometry/S.h>
 
-CTEST(P, test_true){
+CTEST(P, test1){
 	float r = 3.7;
-	const float exp = 2 * r * 3.14;
-    float real = S(r);
+	const float exp = 2 * 3.14 * r;
+    float real = P(r);
     ASSERT_DBL_NEAR(exp, real);
 
 }
 
-CTEST(S, test_true){
+CTEST(S, test2){
 	float r = 3.7;
 	const float exp = r * r * 3.14;
     float real = S(r);
@@ -21,11 +21,11 @@ CTEST(S, test_true){
 
 }
 
-CTEST(intersection, test_true){
+CTEST(intersection, test3){
 	float x1 = 1, y1 = 1, r1 = 5, x2 = 2, y2 = 2, r2 = 5;
 	float exp = 0;
     if ((x2-x1)*(x2-x1) - (y2-y1)*(y2-y1) < (r1+r2)*(r1+r2)){
-        const float exp = 1;
+        exp = 1;
     }
     float real = intersection(x1, y1, r1, x2, y2, r2);
     ASSERT_DBL_NEAR(exp, real);
